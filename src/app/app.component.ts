ef798b46e10aa3b6b4be52aa4d79fd97fa79dba4
Kingshuk Basak <kingshukbasak.1992@gmail.com>
2019-06-15 16:10:52 +0530

    fifth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,25 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontEnd';
-  public imagePath;
-  imgURL: any;
-  public message: string;
+
  
-  preview(files) {
-    if (files.length === 0)
-      return;
- 
-    var mimeType = files[0].type;
-    if (mimeType.match(/image\/*/) == null) {
-      this.message = "Only images are supported.";
-      return;
-    }
- 
-    var reader = new FileReader();
-    this.imagePath = files;
-    reader.readAsDataURL(files[0]); 
-    reader.onload = (_event) => { 
-      this.imgURL = reader.result; 
-    }
-  }
+  
 }
