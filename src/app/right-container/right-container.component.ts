17e7f842ff587d9a66ab1868a11444c4908d628c
Kingshuk Basak <kingshukbasak.1992@gmail.com>
2019-06-15 14:53:32 +0530

    forthpush

---
+++ src/app/right-container/right-container.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-right-container',
+  templateUrl: './right-container.component.html',
+  styleUrls: ['./right-container.component.css']
+})
+export class RightContainerComponent implements OnInit {
+
+  constructor() { }
+  public imagePath;
+  imgURL: any;
+  public message: string;
+
+  ngOnInit() {
+  }
+
+  preview(files) {
+    if (files.length === 0)
+      return;
+ 
+    var mimeType = files[0].type;
+    if (mimeType.match(/image\/*/) == null) {
+      this.message = "Only images are supported.";
+      return;
+    }
+ 
+    var reader = new FileReader();
+    this.imagePath = files;
+    reader.readAsDataURL(files[0]); 
+    reader.onload = (_event) => { 
+      this.imgURL = reader.result; 
+    }
+  }
+
+}
